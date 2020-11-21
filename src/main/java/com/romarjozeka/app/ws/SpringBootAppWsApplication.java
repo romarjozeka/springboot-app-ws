52865b08e86ff2a0875457737e77418449f14c08
Romarjo Zeka <romarjozeka@gmail.com>
2020-11-21 23:57:52 +0000

    Making app deployable to tomcat

--- src/main/java/com/romarjozeka/app/ws/SpringBootAppWsApplication.java
+++ src/main/java/com/romarjozeka/app/ws/SpringBootAppWsApplication.java
@@ -3,11 +3,18 @@ package com.romarjozeka.app.ws;
 import com.romarjozeka.app.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootAppWsApplication {
+public class SpringBootAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppWsApplication.class, args);

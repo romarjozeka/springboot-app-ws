bda3e4c54e54cc087d3950043b268463d8c6c8b6
Romarjo Zeka <romarjozeka@gmail.com>
2020-12-05 16:23:51 +0000

    H2 memory db setup

--- src/main/java/com/romarjozeka/app/ws/security/SecurityConstants.java
+++ src/main/java/com/romarjozeka/app/ws/security/SecurityConstants.java
@@ -12,6 +12,7 @@ public class SecurityConstants {
     public static final String EMAIL_VERIFICATION_URL = "/users/email-verification";
     public static final String PASSWORD_REQUEST_URL = "/users/password-reset-request";
     public static final String PASSWORD_RESET_URL="/users/password-reset";
+    public static final String H2_CONSOLE = "/h2-console/**";
 
     public static String getTokenSecret() {
 
